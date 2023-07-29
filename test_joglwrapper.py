6f671cfa234bf0f760b5763af92eaba6561cd54b
NoxiousFumes <55012494+NoxiousFumes@users.noreply.github.com>
2023-07-29 11:05:21 -0400

    added test file to gitignore

--- test_joglwrapper.py
+++ test_joglwrapper.py
@@ -9,8 +9,9 @@ reader.save_project(5)
 reader.save_members(5)
 reader.save_project(3)
 reader.save_members(3)
-
 reader.save_member_needs(1)
+reader.save_member_needs(5)
+reader.save_member_needs(3)
 
 project = Project(1)
 
