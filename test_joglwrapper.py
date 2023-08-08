fae28fcf3ee8b83589b67035d853ab8d7762e465
NoxiousFumes <55012494+NoxiousFumes@users.noreply.github.com>
2023-08-08 15:07:28 -0400

    Update test_joglwrapper.py

--- test_joglwrapper.py
+++ test_joglwrapper.py
@@ -3,15 +3,12 @@ from joglwrapper import Project
 from joglwrapper import Output
 from pathlib import Path
 
-# reader = Reader()
-# reader.save_all()
+reader = Reader()
+reader.save_all()
 
 output = Output()
 
-output.generate_project(1, 'test')
+for i in range(1,7):
+    output.generate_project(i, f"project_{i}")
+    output.generate_all_users(i)
 
-output.generate_all_users(1)
-
-output.generate_user(671, 'test')
-
-project = Project(1)
