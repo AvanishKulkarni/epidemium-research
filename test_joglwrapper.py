ef7557837bcce564f168ddd0ea80573f5b20f56e
NoxiousFumes <55012494+NoxiousFumes@users.noreply.github.com>
2023-08-22 17:33:05 -0400

    added excel output as csv

--- test_joglwrapper.py
+++ test_joglwrapper.py
@@ -3,48 +3,39 @@ from joglwrapper import Project
 from joglwrapper import Output
 from pathlib import Path
 
-# from pyvis.network import Network
+import csv
 
-# net = Network(height='1000px', width='100%', bgcolor='darkgrey')
-# net.barnes_hut(spring_length=1, spring_strength=0.1, damping=0.5, central_gravity=0.7)
+with open("output.csv", 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f)
 
-# # reader = Reader()
-# # reader.save_all()
+    for index in range(1, 7):
+        project = Project(index)
 
-# # output = Output()
-# # output.generate_meta()
+        writer.writerow(
+            [
+                f'{project.title} ({project.id})',
+                f'{project.short_description}'
+            ]
+        )
+
+        for member in project.get_members():
+            writer.writerow(
+                [
+                    '',
+                    f'{str(member)}',
+                ]
+            )
 
 
-# for index in range(1, 7):
-#     project = Project(index)
+            for activity in member.get_activities():
+                writer.writerow(
+                    [
+                        '','',
+                        f'{activity.type}',
+                        f'{activity.title} ({activity.id})',
+                        f'{activity.summary}'
+                    ]
+                )
+            
 
-#     # Node IDs for members and projects are negative to prevent index conflicts with activities
 
-#     net.add_node(n_id=-1*index, 
-#                  label=f'Project {index}',
-#                  color='aqua',
-#                  mass=4,
-#                  shape='database',
-#                  title=f"<p>{project.title} (id: {project.id})<br>{project.short_description}</p>",
-#                  value=500
-#                  )
-
-#     for member in project.get_members():
-#         net.add_node(n_id=-1*member.id, 
-#                      label=f'{str(member)}',
-#                      color='grey',
-#                      shape='diamond',
-#                      title=f'<p><b>{str(member)}</b></p><p>Affiliation: {member.affiliation}</p><p>Bio: {member.short_bio}</p>'
-#                      )
-#         net.add_edge(-1*member.id, -1*index)
-
-#         for activity in member.get_activities():
-
-#             net.add_node(n_id=activity.id,
-#                          label=activity.type, 
-#                          title=f"<p><b>{activity.type}: {activity.title}:</b></p><p>{activity.summary}</p>",
-#                          shape="circle",
-#                          color="green")
-#             net.add_edge(-1*member.id, activity.id)
-
-# net.show('test.html', notebook=False)
